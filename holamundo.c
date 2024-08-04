61c4e7ab4bae09a737471a868cb85bc08f2d0836
Inyerman <3000305680101@ingenieria.usac.edu.gt>
2024-08-03 19:03:53 -0600

    primer comentario

---
+++ holamundo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	printf("-----------------\n");
+	printf("Inyerman Alexander Xap Chin\n");
+	printf("202100297\n");
+	printf("------------------\n");
+	return 0;
+}
