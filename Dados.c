6c4d095414cd7e09300f5373b88b40a1e16e9481
Inyerman <3000305680101@ingenieria.usac.edu.gt>
2024-08-16 15:40:34 -0600

    primer comentario

---
+++ Dados.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main () {
+    double = dado1, dado2;
+   
+
+
+    printf("Juego del 8\n");
+    printf("Reglas lanzas 2 dados y si sumas 8 ganas\n");
+    printf("Si sacas 7 pierdes\n");
+    printf("Si sacas cualquier otro numero puedes seguir jugando\n");
+    double = dado1 = rand %7;
+    double = dado2 = rand %7;
+    printf("Dado 1: %lf\n", dado1);
+    return 0;
+}
