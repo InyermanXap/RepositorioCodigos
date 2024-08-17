6c6bc16bf30b2b9ff6c8537dce24598e19e09097
InyermanXap <3000305680101@ingenieria.usac.edu.gt>
2024-08-16 18:29:37 -0600

    Update Dados.c
    ya funciona

--- Dados.c
+++ Dados.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 int main () {
-    double = dado1, dado2;
-   
-
-
+    int dado1, dado2, resultado;
+    srand(time(NULL));
+    dado1=rand() % 6 + 1;
+    dado2=rand() % 6 + 1;
+    resultado = dado1 + dado2;
+    /*(0 a 6)*/
     printf("Juego del 8\n");
     printf("Reglas lanzas 2 dados y si sumas 8 ganas\n");
     printf("Si sacas 7 pierdes\n");
     printf("Si sacas cualquier otro numero puedes seguir jugando\n");
-    double = dado1 = rand %7;
-    double = dado2 = rand %7;
-    printf("Dado 1: %lf\n", dado1);
+    /*Reglas del juego */
+    printf("Tus dados dieron:\n");
+    printf("Dado 1: %i\n", dado1);
+    printf("Dado 2: %i\n", dado2);
+    printf("Resultado: %i\n", resultado);
+    if(resultado==7) {
+       printf("Lo lamento perdiste\n");
+    }
+    if(resultado==8) {
+            printf("Felicidades Ganaste\n");
+        }
+        else {
+            printf("Sigue participando\n");
+        }
     return 0;
 }
